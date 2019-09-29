583f5c181989ca8e91acf58cc657d17e6d1b041b
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-09-29 20:59:32 -0300

    remove unnecessary folder

--- ScratchBotApp.ts
+++ ScratchBotApp.ts
@@ -9,14 +9,14 @@ import {
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IMessage, IPostMessageSent } from '@rocket.chat/apps-engine/definition/messages';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
-import { AppSetting, settings } from './src/rocketchat/config/settings';
-import { LivechatMessageHandler } from './src/rocketchat/handlers/livechat-messages.handler';
-import { MessageHelper } from './src/rocketchat/helpers/message.helper';
-import { RoomHelper } from './src/rocketchat/helpers/room.helper';
-import { SettingsHelper } from './src/rocketchat/helpers/settings.helper';
-import { StorageHelper } from './src/rocketchat/helpers/storage.helper';
-import { UserHelper } from './src/rocketchat/helpers/user.helper';
-import { NluSdk } from './src/rocketchat/nlu-sdk/nlu-sdk';
+import { AppSetting, settings } from './src/config/settings';
+import { LivechatMessageHandler } from './src/handlers/livechat-messages.handler';
+import { MessageHelper } from './src/helpers/message.helper';
+import { RoomHelper } from './src/helpers/room.helper';
+import { SettingsHelper } from './src/helpers/settings.helper';
+import { StorageHelper } from './src/helpers/storage.helper';
+import { UserHelper } from './src/helpers/user.helper';
+import { NluSdk } from './src/nlu-sdk/nlu-sdk';
 
 export class ScratchBotApp extends App implements IPostMessageSent {
 	constructor(info: IAppInfo, logger: ILogger) {
